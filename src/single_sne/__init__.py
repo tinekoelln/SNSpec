3ed11741a64bee57b193a4fd852312cbb68657be
Cristine Koelln <cristine.koelln@eso.org>
2025-12-02 17:59:14 +0100

    __init__: modified __init__ to have the correct library name

--- src/single_sne/__init__.py
+++ src/single_sne/__init__.py
@@ -5,16 +5,16 @@
 import importlib
 
 def __getattr__(name):
-    snspec = importlib.import_module(".snspec", __name__)
+    snspec = importlib.import_module(".single_sne", __name__)
     return getattr(snspec, name)
 
 def __dir__():
-    snspec = importlib.import_module(".snspec", __name__)
+    snspec = importlib.import_module(".single_sne", __name__)
     return sorted(set(globals().keys()) | set(dir(snspec)))
 
 # Mirror __all__ from snspec (if defined) so that help(single_sne) looks nice.
 try:
-    _snspec = importlib.import_module(".snspec", __name__)
+    _snspec = importlib.import_module(".single_sne", __name__)
     __all__ = getattr(_snspec, "__all__", [n for n in dir(_snspec) if not n.startswith("_")])
 except Exception:
     # During editable install, snspec might fail temporarily; keep namespace minimal.
