1832024f96bdcc7fc7da1813b821cc54252c7fce
Cristine Koelln <cristine.koelln@eso.org>
2025-12-02 15:40:15 +0100

    added input/output for photometric data

---
+++ src/single_sne/io/lightcurves.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import astropy.units as u
+from pathlib import Path
+from typing import Iterable
+
+
+def _iter_files(d: Path, patterns: Iterable[str] | str = ("*.dat", "*.fits")):
+    # Coerce a single string into a tuple of one pattern
+    if isinstance(patterns, (str, Path)):
+        patterns = (str(patterns),)
+    for pat in patterns:
+        for p in d.glob(pat):
+            if p.is_file():
+                yield p
+                
+                
+def find_lightcurve(root, debug=False):
+    #receives root folder, finds light_curve folder, returns list of light_curves
+    lcdir = root/ "lightcurves"
+    if not lcdir.is_dir(): lcdir = root/ "light_curves" 
+    
+    curves_list = []
+    if lcdir.is_dir():
+        for datafile in _iter_files(lcdir, patterns =( "sn*.txt")):
+            curves_list.append(datafile)
+            
+    return lcdir, curves_list
+    
+
+def read_lightcurve(path, debug=False):
+    # Receives path to the lightcurve, outputs dataframe with mjd, mag, err, filter, substracted
+    lightcurve_df = pd.read_csv(path, sep = ' ')
+    return lightcurve_df
